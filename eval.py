d4b428eca3e2bb100087f35e5032f3cb8849fd9b
Katerina-Shush <116030372+Katerina-Shush@users.noreply.github.com>
2023-02-23 22:14:52 +0300

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,4 +1,38 @@
 from lsp import lstsq
+import numpy as np
 
-if __name__ == "__main__":
+def lstsq_ne(A, b):
+    assert len(A.shape) == len(b.shape) + 1, "Can's solve for shapes {} and {}".format(len(A.shape), len(b.shape))
+    assert A.shape[0] == b.shape[0], "Can's solve for shapes {} and {}".format(A.shape, b.shape)
+    
+    AA = np.linalg.inv(np.dot(A.T, A))
+    Ab = np.dot(A.T, b)
+    x = np.dot(AA, Ab)
+    r = np.dot(A, x) - b
+    cost = np.dot(r.T, r)
+    sigma = cost / (A.shape[0] - A.shape[1])
+    var = A * sigma * sigma
+    return x, cost, var
+
+def lstsq_svd(A, b):
     pass
+
+def lstsq(A, b, method, **kwargs):
+    if method == 'ne':
+        return lstsq_ne(A, b)
+    else:
+        return lstsq_svd(A, b, **kwargs)
+
+if __name__ == '__main__':
+    A = np.random.normal(size=(500, 20))
+    params_x = np.random.normal(size=(20,))
+    b = A @ params_x + 0.01*np.random.normal(size=(500,))
+
+    x1, x2, x3 = lstsq_ne(A, b)
+    print(x1)
+    print('________________')
+    print(x2)
+    print('________________')
+    #print(x3)
+    print('________________')
+
